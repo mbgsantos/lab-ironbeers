8a30f6bb48b1f284782d03e3b024ecbcc3849ae9
Marcelo Santos <marcelo.b.g.santos@gmail.com>
2023-05-28 21:40:47 +0100

    updated lab

--- app.js
+++ app.js
@@ -12,14 +12,43 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
 // Register the location for handlebars partials here:
 
 // ...
 
 // Add the route handlers here:
 
+// Promises
+/* app.get('/beers', (req, res) => {
+  punkAPI.getBeers().then(beers => {
+    // console.log(beers);
+    res.render('beers', { beers });
+  });
+}); */
+
+app.get('/beers', async (req, res) => {
+  const beers = await punkAPI.getBeers();
+  // res.render('partials/beerpartial', { beers });
+  res.render('beers', { beers });
+});
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+app.get('/random-beer', (req, res) => {
+  const randomBeer = punkAPI.getRandom();
+
+  randomBeer.then(beer => {
+    res.render('random-beer', { beer });
+  });
+});
+
+/* app.get('/random-beer', async (req, res) => {
+  const randomBeer = await punkAPI.getRandom();
+  res.render('randomBeer', { randomBeer });
+}); */
